5bfd273d39b252e9944707926e49206fe6286110
Paweł Redzyński <pawelredzynski@gmail.com>
2022-06-17 16:46:06 +0200

    renderers: account for space in plots name

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -4,7 +4,7 @@ from dvc_render.base import Renderer
 
 
 def test_remove_special_characters():
-    special_chars = r"!@#$%^&*()[]{};,<>?\/:.|`~=_+"
+    special_chars = r"!@#$%^&*()[]{};,<>?\/:.|`~=_+ "
     dirty = f"plot_name{special_chars}"
     assert Renderer.remove_special_chars(dirty) == "plot_name" + "_" * len(
         special_chars
