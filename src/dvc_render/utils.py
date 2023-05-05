7bf6e94698d2b602a7a31c481a406c3a0b1d8ce6
dberenbaum <dave@iterative.ai>
2023-05-05 17:23:41 -0400

    table: show nested metrics as separate columns

--- src/dvc_render/utils.py
+++ src/dvc_render/utils.py
@@ -1,5 +1,21 @@
+from collections.abc import MutableMapping
+
+
 def list_dict_to_dict_list(list_dict):
     """Convert from list of dictionaries to dictionary of lists."""
     if not list_dict:
         return {}
-    return {k: [x[k] for x in list_dict] for k in list_dict[0]}
+    flat_list_dict = [flatten(d) for d in list_dict]
+    return {k: [x[k] for x in flat_list_dict] for k in flat_list_dict[0]}
+
+
+# https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys
+def flatten(d, parent_key="", sep="."):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
